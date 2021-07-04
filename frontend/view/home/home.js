ecbd7d8fd3702e71ba32cc7686b0e2d77df95929
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-07-04 11:08:36 +0200

    create js files for each page

---
+++ frontend/view/home/home.js
@@ -0,0 +1,12 @@
+class Product{
+    name;
+    price;
+    description;
+    constructor(name, price, description){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
+
+let Product = new Product("Cross Table", "59900€", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
