fca038157e9d760be3374f716d2b91f9bb4c5a60
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-07-09 11:17:01 +0200

    generates product card of the product selected

--- frontend/view/product/product.js
+++ frontend/view/product/product.js
@@ -10,7 +10,36 @@ window.onload = async () => {
     fetchProductById(getId());
 }
 
+// Get url id
+
 function getId(){
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get("id");
+}
+
+// Generates product selected
+
+function createCard(product){
+    const divProduct = document.createElement("div");
+    divProduct.innerHTML = `<div class="card">
+                                <img src="${product.imageUrl}" class="img-fluid card-img-top" alt="product image"></img>
+                                <div class="card-body">
+                                    <h2 class="card-title">${product.name}</h2>
+                                    <p class="card-text">${product.description}</p>
+                                    <p>${product.price/100}€</p>
+                                    <div>
+                                        <label for="varnish-choice">Vernis :</label>
+                                        <select name="varnish" id="varnish-choices>
+
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>`
+    return divProduct;
+}
+
+function showProduct(productData){
+    const card = createCard(productData);
+    const selectedProductContainer = document.getElementById("selected-product");
+    selectedProductContainer.append(card);
 }
