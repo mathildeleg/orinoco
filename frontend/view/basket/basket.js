0e48d448bdb1fafbdc4d659d5657f38a36e6a738
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2021-07-19 11:38:32 +0200

    calculate total price of basket

--- frontend/view/basket/basket.js
+++ frontend/view/basket/basket.js
@@ -8,32 +8,69 @@ window.onload = async () => {
     getBasketContent();
 }
 
-// Generates basket content and its card
+// Creates basket content and its card
 function createBasketCard(basketContent){
     const basketCard = document.createElement("div");
     basketCard.innerHTML = `<div class="card">
                                 <div class="card-body">
                                     <img src="${basketContent.image}" class="img-fluid card-img-top" alt="product image"></img>
                                     <h2 class="card-title">${basketContent.name}</h2>
-                                    <p>${basketContent.price}€</p>
-                                    <p>${basketContent.varnish}</p>
+                                    <p>Vernis : ${basketContent.varnish}</p>
+                                    <p>Prix : ${basketContent.price}€</p>
                                 </div>
                             </div>`;
     return basketCard;
 }
 
+// Generates basket cards
 function showBasket(basketItemList){
     const card = basketItemList && basketItemList.map(createBasketCard);
     const basketCards = document.getElementById("basket-container");
+    // If empty, show empty div
     if(card === null){
         const emptyBasket = `<div class="card">
                                 <div class="card-body">Votre panier est vide</div>
                             </div>`;
         basketCards.innerHTML = emptyBasket;
         return emptyBasket;
+    // If filled with products, show list of products in basket
     }else{
         card.forEach(div => {
         basketCards.append(div);
         })
-    }   
+        // Put price of each product in an array
+        let sumPrice = [];
+        for(let i = 0; i < basketItemList.length; i++){
+        const productsPrices = basketItemList[i].price;
+        sumPrice.push(productsPrices)
+        }
+        // Calculate total of basket
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        const totalPrice = sumPrice.reduce(reducer, 0);
+        // Create and insert totalPrice HTML
+        const totalPriceDiv = `<div class="card">
+                                    <div class="card-body">Prix total de votre panier : ${totalPrice} €</div>
+                                </div>`
+        basketCards.insertAdjacentHTML("beforeend", totalPriceDiv);
+    }
 }
+
+// function createArrayOfPrices(){
+// let sumPrice = [];
+// for(let i = 0; i < basketItemList.length; i++){
+//     const productsPrices = basketItemList[i].price;
+//     sumPrice.push(productsPrices)
+// }
+// }
+
+// function calculateTotalPrice(){
+// const reducer = (accumulator, currentValue) => accumulator + currentValue;
+// const totalPrice = sumPrice.reduce(reducer, 0);
+// }
+
+// function insertTotalPrice(){
+//     const totalPriceDiv = `<div class="card">
+//                                     <div class="card-body">Prix total de votre panier : ${totalPrice} €</div>
+//                                 </div>`
+//     basketCards.insertAdjacentHTML("beforeend", totalPriceDiv);
+// }
